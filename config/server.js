1bbcc93ea74f3e08ef3cb13051259d293c1bca58
RobinYzewyn <robin.yzewyn@student.howest.be>
2021-03-29 10:48:51 +0200

    lol

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '421276998927eeb17e7344cc2ed2781d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aebc7888-4da5-4039-9a87-0a3f3e01ae33'
+};
